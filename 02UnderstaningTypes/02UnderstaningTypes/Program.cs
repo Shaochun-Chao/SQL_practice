95217fad6657885d3d551a196ce1125fa3350a78
evol5523 <33061291+evol5523@users.noreply.github.com>
2022-06-28 23:50:42 -0700

    0.0

---
+++ 02UnderstaningTypes/02UnderstaningTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//sbyte, byte, short, ushort, int, uint, long,ulong, float, double, and decimal.
+Console.WriteLine("sbyte Max: {0}, Min :{1}", sbyte.MaxValue, sbyte.MinValue);
+Console.WriteLine("byte Max :{0}, Min :{1}", byte.MaxValue, byte.MinValue);
+Console.WriteLine("short Max :{0}, Min :{1}", short.MaxValue, short.MinValue);
+Console.WriteLine("ushort Max :{0}, Min :{1}", ushort.MaxValue, ushort.MinValue);
+Console.WriteLine("int Max :{0}, Min :{1}", int.MaxValue, int.MinValue);
+Console.WriteLine("uint Max :{0}, Min :{1}", uint.MaxValue, uint.MinValue);
+Console.WriteLine("long Max :{0}, Min :{1}", long.MaxValue, long.MinValue);
+Console.WriteLine("ulong Max :{0}, Min :{1}", ulong.MaxValue, ulong.MinValue);
+Console.WriteLine("float Max :{0}, Min :{1}", float.MaxValue, float.MinValue);
+Console.WriteLine("double Max :{0}, Min :{1}", double.MaxValue, double.MinValue);
+Console.WriteLine("decimal Max :{0}, Min :{1}", decimal.MaxValue, decimal.MinValue);
+
+while (true)
+{
+    int num;
+    Console.WriteLine("Input");
+    //Centruies
+
+    num = Convert.ToInt16(Console.ReadLine());
+
+    ushort Years = (ushort)(num * 100);
+
+    float Days = (float)(Years * 365.24);
+
+    float Hours = (float)(Days * 24);
+
+    double Minutes = Hours * 60;
+
+    double Seconds = Minutes * 60;
+
+    decimal Milliseconds = (decimal)(Seconds * 1000);
+
+    decimal Microseconds = (Milliseconds * 1000);
+
+    decimal Nanoseconds = (Microseconds * 1000);
+
+    Console.WriteLine($"{num} Centruies = {Years} Years = {Days} Days = {Hours} Hours = {Minutes} Minutes = {Seconds} Seconds = {Milliseconds} Milliseconds = {Microseconds} Microseconds = {Nanoseconds} Nanoseconds");
+}
+
---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/***for (int i = 1; i <= 100; i++){
+    if (i % 15 == 0){
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0){
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0){
+        Console.WriteLine("Buzz");
+    }
+    else{
+        Console.WriteLine(i);
+    }
+}***/
+/***
+int max = 500; 
+for (byte i = 0; i < max; i++) {
+    Console.WriteLine(i); 
+}***/
+//infinity loop
+
+/***for (int i = 1; i<=5; i++){
+    for (int j = 1; j<=5-i; j++){
+        Console.Write(" ");
+    }
+    for (int s = 1; s<= 2*i-1; s++){
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}***/
+
+//3.
+/***Random ran = new Random();
+int target = ran.Next(0, 4);
+
+
+while (true)
+{
+    Console.WriteLine("Guess a number between 1 and 3");
+    int guess = Convert.ToInt32(Console.ReadLine());
+    if (guess >= 1 and guess <= 3)
+    {
+        if (guess == target){
+            Console.WriteLine("Correct");
+            break;
+        }
+        else if (guess < target)
+            Console.WriteLine("Too low");
+        else
+            Console.WriteLine("Too high");
+
+    }
+    else
+    {
+        Console.WriteLine("Out of range");
+    }
+}***/
+
+/***DateTime Cur = DateTime.Today;
+
+Console.WriteLine("Enter the month: ");
+int month = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the year: ");
+int year = Convert.ToInt32(Console.ReadLine());
+
+DateTime Born = new DateTime(year, month, day);
+
+TimeSpan diff = Cur.Subtract(Born);
+Console.WriteLine(diff);***/
+
+/***
+DateTime Current = DateTime.Now;
+int curr = Current.Hour;
+int Morning = 5;
+int Afternoon = 12;
+int Evening = 18;
+int Night = 22;
+
+if (curr >= Morning  && curr < Afternoon)
+{
+    Console.WriteLine("Good Morning");
+}
+else if (curr >= Afternoon && curr < Evening)
+{
+    Console.WriteLine("Good Afternoon!");
+}
+else if (curr >= Evening && curr < Night)
+{
+    Console.WriteLine("Good Evening!");
+}
+else 
+{
+    Console.WriteLine("Good Night!");
+}
+***/
+
+for (int i = 1; i <= 4; i += 1)
+{
+    for (int j = 0; j <= 24; j += i)
+    {
+        if ( j < 24)
+         Console.Write($"{j},");
+        else
+            Console.Write($"{j}");
+    }
+
+    Console.WriteLine();
+}
